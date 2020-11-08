02d9c436d68593ec6d9274aa23f2b13a457fdd06
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-07 22:08:08 -0300

    feat: WebRTC test

--- src/containers/index/index.tsx
+++ src/containers/index/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styled from 'styled-components';
-import Navbar from '../../components/layout/navbar';
 import Footer from '../../components/layout/footer';
+import Navbar from '../../components/layout/navbar';
 
 const Main = styled.div`
 	width: 100%;
@@ -21,13 +21,83 @@ const JSONLD = `{
 	"image": "/images/logo.png"
 }`;
 
+const localConnection = new RTCPeerConnection();
+const remoteConnections: RTCDataChannel[] = [];
+
+const getICEs = () => {
+
+	localConnection.onicecandidate = () => {
+		console.log(' NEW ice candidnat!! on localconnection reprinting SDP ');
+		console.log(JSON.stringify(localConnection.localDescription));
+	};
+
+
+	const sendChannel = localConnection.createDataChannel('sendChannel');
+	sendChannel.onmessage = e => console.log('messsage received!!!' + e.data);
+	sendChannel.onopen = () => console.log('open!!!!');
+	sendChannel.onclose = () => console.log('closed!!!!!!');
+
+
+	localConnection.createOffer().then(o => localConnection.setLocalDescription(o));
+
+};
+
+getICEs();
+
+const setupRemoteConnection = (offer: any) => {
+
+	const remoteConnection = new RTCPeerConnection();
+
+	remoteConnection.onicecandidate = () => {
+		console.log(' NEW ice candidnat!! on localconnection reprinting SDP ');
+		console.log(JSON.stringify(remoteConnection.localDescription));
+	};
+
+
+	remoteConnection.ondatachannel = event => {
+
+		const receiveChannel = event.channel;
+		receiveChannel.onmessage = e => console.log('messsage received!!!' + e.data);
+		receiveChannel.onopen = () => {
+			console.log('open!!!!');
+		};
+		receiveChannel.onclose = () => console.log('closed!!!!!!');
+		remoteConnections.push(receiveChannel);
+
+	};
+
+
+	remoteConnection.setRemoteDescription(offer).then(() => console.log('done'));
+
+	// create answer
+	remoteConnection.createAnswer().then(a => remoteConnection.setLocalDescription(a)).then(() =>
+		console.log(JSON.stringify(remoteConnection.localDescription)));
+	// send the anser to the client
+};
+
+const p2pConnection = () => {
+	const iceInput = document.getElementById('ice') as HTMLInputElement;
+	const ice = iceInput.value;
+	iceInput.value = '';
+	setupRemoteConnection(JSON.parse(ice));
+};
+
+const sendP2PText = () => {
+	const textInput = document.getElementById('ice') as HTMLInputElement;
+	const text = textInput.value;
+	textInput.value = '';
+	remoteConnections.forEach(remoteConnection => remoteConnection.send(text));
+};
+
 export default function Home() {
+
+
 	return (
 		<>
 			<Head>
 				<title>My Home Page Title</title>
 				{/* TODO - add real url */}
-				<link rel="canonical" href="https://my-domain.com/home"/>
+				<link rel="canonical" href="https://my-domain.com/home" />
 
 				{/* This is json-ld with schema data */}
 				<script type='application/ld+json'>{JSONLD}</script>
@@ -35,6 +105,11 @@ export default function Home() {
 			<Navbar />
 			<Main>
 				<h1>Hello!</h1>
+				<input id="ice" type="text" />
+				<button onClick={p2pConnection}>Connect (ICE)</button>
+				<br/>
+				<input id="text" type="text"/>
+				<button onClick={sendP2PText} >Send to connections</button>
 			</Main>
 			<Footer />
 		</>
