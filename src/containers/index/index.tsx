229d8d2efbb3dca677af1725dcbb6994f12bb228
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-25 20:23:18 -0300

    feat: better logs

--- src/containers/index/index.tsx
+++ src/containers/index/index.tsx
@@ -42,7 +42,7 @@ export const Home = () => {
 	const [messages, setMessages] = useArrayState<string>([]);
 
 	// const inputIce = useRef<HTMLInputElement>(null);
-	const inputAnswer = useRef<HTMLInputElement>(null);
+	// const inputAnswer = useRef<HTMLInputElement>(null);
 	const inputText = useRef<HTMLInputElement>(null);
 
 	React.useEffect(() => {
@@ -69,7 +69,7 @@ export const Home = () => {
 				continue;
 			}
 			connections.push(user); // TODO: set it as pending
-			console.log('connection to new user', user);
+			console.log('trying to connect to new user', user);
 			// TODO: how to chose a offer?
 			const offer = ices[user][0];
 			setupRemoteConnection(offer, user);
@@ -78,7 +78,7 @@ export const Home = () => {
 	};
 
 	const confirmAnswer = async () => {
-		const { data: answers } = await firebaseFunctions.get('getAnswer?user='+userName);
+		const { data: answers } = await firebaseFunctions.get('getAnswer?user=' + userName);
 		for (const answer of Object.values<string>(answers)) {
 			await p2pAnswerCallback(answer);
 		}
@@ -87,12 +87,12 @@ export const Home = () => {
 	const getICEs = () => {
 		localConnection = new RTCPeerConnection();
 		localConnection.onicecandidate = () => {
-			console.log(' NEW ice candidnat!! on localconnection reprinting SDP ');
 
 			while (!userName) {
 				userName = prompt('What\'s your name?')!;
 			}
 			setUserName(userName);
+			console.log('Novo ICE local, mandando para o server');
 			firebaseFunctions.post('saveIce', {
 				name: userName,
 				ice: localConnection.localDescription
@@ -106,13 +106,15 @@ export const Home = () => {
 
 		const sendChannel = localConnection.createDataChannel('sendChannel');
 		sendChannel.onmessage = e => {
-			console.log('messsage received!!! ' + e.data);
 			messages.push(e.data);
 			setMessages(messages);
 		};
 
-		sendChannel.onopen = () => console.log('open!!!!');
-		sendChannel.onclose = () => console.log('closed!!!!!!');
+		sendChannel.onopen = () => console.log('Send channel is open!!!!');
+		sendChannel.onclose = () => {
+			firebaseFunctions.post('clearIces', { name: userName });
+			console.log('Send channel is closed!!!!!!');
+		};
 
 
 		localConnection.createOffer().then(o => localConnection.setLocalDescription(o));
@@ -124,9 +126,8 @@ export const Home = () => {
 		const remoteConnection = new RTCPeerConnection();
 
 		remoteConnection.onicecandidate = () => {
-			console.log(' NEW answer! on localconnection reprinting SDP ');
+			console.log('Resposta de uma conexao remota, mandando para o server');
 			const newAnswer = JSON.stringify(remoteConnection.localDescription);
-			console.log(newAnswer);
 			firebaseFunctions.post('answer', { from: userName, to: otherUserName, answer: newAnswer });
 			setAnswerText('\n' + newAnswer);
 		};
@@ -135,10 +136,11 @@ export const Home = () => {
 		remoteConnection.ondatachannel = event => {
 
 			const receiveChannel = event.channel;
+			// TODO: oq fazer com esse evento?
 			receiveChannel.onmessage = e => console.log('messsage received!!! ' + e.data);
 			receiveChannel.onopen = () => {
-				console.log('open!!!!', receiveChannel);
-				connections.push(otherUserName!);
+				console.log('Conexão remota aberta!!!!');
+				// connections.push(otherUserName!);
 				// setConnections(connections);
 			};
 			receiveChannel.onclose = () => {
@@ -153,11 +155,13 @@ export const Home = () => {
 		};
 
 
-		remoteConnection.setRemoteDescription(offer).then(() => console.log('done'));
+		remoteConnection.setRemoteDescription(offer).then(() => console.log('Set de oferta feito'));
 
 		// create answer
 		remoteConnection.createAnswer().then(a => remoteConnection.setLocalDescription(a)).then(() =>
-			console.log(JSON.stringify(remoteConnection.localDescription)));
+			// console.log(JSON.stringify(remoteConnection.localDescription);
+			0
+		);
 		// send the anser to the client
 	};
 
@@ -171,25 +175,25 @@ export const Home = () => {
 	const p2pAnswerCallback = (answerText: string) => {
 		const answer = JSON.parse(answerText);
 		localConnection.setRemoteDescription(answer).then(
-			() => console.log('done')
+			() => console.log('Resposta confirmada!')
 		);
 	};
 
-	const p2pAnswerOnClick = () => {
-		if (inputAnswer.current?.value) {
-			const answer = JSON.parse(inputAnswer.current.value);
-			inputAnswer.current.value = '';
-			localConnection.setRemoteDescription(answer).then(
-				() => console.log('done')
-			);
-		}
-	};
+	// const p2pAnswerOnClick = () => {
+	// 	if (inputAnswer.current?.value) {
+	// 		const answer = JSON.parse(inputAnswer.current.value);
+	// 		inputAnswer.current.value = '';
+	// 		localConnection.setRemoteDescription(answer).then(
+	// 			() => console.log('done')
+	// 		);
+	// 	}
+	// };
 
 	const sendP2PText = () => {
 		if (inputText.current?.value) {
 			const text = inputText.current.value;
 			inputText.current.value = '';
-			console.log('sendP2PText -> remoteConnections', remoteConnections);
+			console.log('conexoes ao enviar mensagem', remoteConnections);
 			const message = `${userName}: ${text}`;
 			messages.push(message);
 			setMessages(messages);
@@ -212,12 +216,12 @@ export const Home = () => {
 			<Navbar />
 			<Main>
 				<h1>Hello {userName}</h1>
-				{/* <br /> */
-				/* <input ref={inputIce} type="text" />
-				<button onClick={p2pConnection}>Connect (ICE)</button> */}
+				{/* <br />
+				<input ref={inputIce} type="text" />
+				<button onClick={p2pConnection}>Connect (ICE)</button>
 				<br />
 				<input ref={inputAnswer} type="text" />
-				<button onClick={p2pAnswerOnClick}>Answer (ICE)</button>
+				<button onClick={p2pAnswerOnClick}>Answer (ICE)</button> */}
 				<br />
 				<input ref={inputText} type="text" />
 				<button onClick={sendP2PText} >Send to connections</button>
